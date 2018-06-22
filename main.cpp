e7ab90f5cfe8b55358e88432ca342dc5999f357a
knoic <40415525+knoic@users.noreply.github.com>
2018-06-22 15:55:14 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ struct zggz
 	float pay_yf;          //应发工资
 	float pay_sf;         //实发工资
 	float duty;             //个人所得税
-}zg_read[100];
+}zggz[100];
 
 
 void cal_duty()           //税计算
@@ -37,18 +37,40 @@ int read()
 	{
 		return -1 ;
 	}
-    fread( (char*)zg_read , sizeof(struct zggz), n , fp ); //从文件中读n个结构体的数据
+    fread( (char*)zggz , sizeof(struct zggz), n , fp ); //从文件中读n个结构体的数据
 	fclose(fp);
     cout<<n<<endl;
-    cout<<zg_read[2].name;
+    cout<<zggz[2].name;
 	return 0;
 }       //读取职工工资数据函数,主函数执行时要调用和必须调用的第一个函数
-void write();        //保存职工工资数据函数
+int write()
+{
+	FILE *fp ;
+    fp=fopen("gz.dat" , "wb" );
+    if ( fp == NULL )
+    {
+		return -1 ;
+	}
+    fwrite( (char*)zggz , sizeof(struct zggz), n , fp ); //将数组写入文件
+	fclose(fp);
+};        //保存职工工资数据函数
 void find();		 //查询职工工资数据函数
 void list();			//浏览职工工资数据函数
 void modify();			//修改职工工资数据函数
 void del();				//删除职工工资数据函数
+void add()
+{
+	int add_num;
+	cout<<"请输入需存入的职工人数："<<endl;
+	cin>>add_num;
+	for(int i=n;i<n+add_num;++i)
+	{
+		cin>>zggz[i].num>>zggz[i].name>>zggz[i].pay_gw>>zggz[i].pay_xinji>>zggz[i].pay_zw>>zggz[i].pay_xiaoji;
+		zggz[i].pay_yf=zggz[i].pay_gw+zggz[i].pay_xiaoji+zggz[i].pay_xinji+zggz[i].pay_zw;
+	}
+		n=n+add_num;
 
+};
 
 
 int main()
@@ -69,8 +91,7 @@ read();
 	{
 	    case 1:cout<<"已进入查询模块";
 			{
-				FILE *fp ;
-				fp=fopen("gz.dat" , "rb" );
+				
                 
 				break;
 			}
@@ -79,19 +100,11 @@ read();
 	    case 3: 
 			{
 			cout<<"已进入添加模块";
-			int n;
-			cout<<"请输入需存入的职工人数："<<endl;
-			cin>>n;
-		
-			struct zggz zg[100];
-			for(int i=0;i<n;++i)
-			{
-				
-				cin>>zg[i].num>>zg[i].name>>zg[i].pay_gw>>zg[i].pay_xinji>>zg[i].pay_zw>>zg[i].pay_xiaoji;
-			}
+			add();
+			
 			
 		
-			cout<<zg[0].pay_gw;
+		/*	cout<<zg[0].pay_gw;
 			FILE *fp ;
             fp=fopen("gz.dat" , "a+" );
             if ( fp == NULL )
@@ -108,7 +121,8 @@ read();
             fclose(fp);
             cout<<zg_read[0].name<<zg_read[0].num<<zg_read[0].pay_gw;
 		    return 0;
-		    break;
+		    
+			*/break;
 		}
 		
 
@@ -122,7 +136,12 @@ read();
 
 	case 4:cout<<"已进入删除模块";break;
 	case 5:cout<<"已进入浏览模块";break;
-	case 6:cout<<"已保存";break;
+	case 6:
+		{
+			cout<<"已保存";
+			write();
+			break;
+		}
 	case 7:
 		{cout<<"系统退出，欢迎再次使用！";
 		   for_main=2;break;
