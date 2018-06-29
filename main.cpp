9d585874823c4c36d30366a86e85f3f772077675
knoic <40415525+knoic@users.noreply.github.com>
2018-06-29 10:16:58 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -46,54 +46,54 @@ void grsds(int i)           //税计算
 {
     printf("税计算：");
 
-	if(zggz[i].pay_yf<=500)
+	if(zggz[i].pay_yf <= 500)
     {
-        zggz[i].duty=zggz[i].pay_yf*0.05;
+        zggz[i].duty = zggz[i].pay_yf*0.05;
     }
 
-    else if(zggz[i].pay_yf<=2000 && zggz[i].pay_yf>500)
+    else if(zggz[i].pay_yf <= 2000 && zggz[i].pay_yf > 500)
     {
-        zggz[i].duty=(zggz[i].pay_yf-500)*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf-500) * 0.1 + 500 * 0.05;
     }
 
-    else if(zggz[i].pay_yf<=5000 && zggz[i].pay_yf>2000)
+    else if(zggz[i].pay_yf <= 5000 && zggz[i].pay_yf > 2000)
     {
-         zggz[i].duty=(zggz[i].pay_yf-2000)*0.15+1500*0.1+500*0.05;
+         zggz[i].duty = (zggz[i].pay_yf - 2000) * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-    else if(zggz[i].pay_yf<=20000 && zggz[i].pay_yf>5000)
+    else if(zggz[i].pay_yf <= 20000 && zggz[i].pay_yf > 5000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-5000)*0.20+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 5000) * 0.20 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-    else if(zggz[i].pay_yf<=40000 && zggz[i].pay_yf>20000)
+    else if(zggz[i].pay_yf <= 40000 && zggz[i].pay_yf > 20000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-20000)*0.25+15000*0.2+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 20000) * 0.25 + 15000 * 0.2 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-	else if(zggz[i].pay_yf<=40000 && zggz[i].pay_yf>20000)
+	else if(zggz[i].pay_yf <= 40000 && zggz[i].pay_yf > 20000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-20000)*0.25+15000*0.2+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 20000) * 0.25 + 15000 * 0.2 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-	else if(zggz[i].pay_yf<=60000 && zggz[i].pay_yf>40000)
+	else if(zggz[i].pay_yf <= 60000 && zggz[i].pay_yf > 40000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-40000)*0.3+20000*0.25+15000*0.2+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 40000) * 0.3 + 20000 * 0.25 + 15000 * 0.2 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-	else if(zggz[i].pay_yf<=80000 && zggz[i].pay_yf>60000)
+	else if(zggz[i].pay_yf <= 80000 && zggz[i].pay_yf > 60000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-60000)*0.35+20000*0.3+20000*0.25+15000*0.2+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 60000) * 0.35 + 20000 * 0.3 + 20000 * 0.25 + 15000 * 0.2 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-	else if(zggz[i].pay_yf<=100000 && zggz[i].pay_yf>80000)
+	else if(zggz[i].pay_yf <= 100000 && zggz[i].pay_yf > 80000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-80000)*0.4+20000*0.35+20000*0.3+20000*0.25+15000*0.2+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 80000) * 0.4 + 20000 * 0.35 + 20000 * 0.3 + 20000 * 0.25 + 15000 * 0.2 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
-	else if(zggz[i].pay_yf>100000)
+	else if(zggz[i].pay_yf > 100000)
     {
-        zggz[i].duty=(zggz[i].pay_yf-100000)*0.45+20000*0.4+20000*0.35+20000*0.3+20000*0.25+15000*0.2+3000*0.15+1500*0.1+500*0.05;
+        zggz[i].duty = (zggz[i].pay_yf - 100000) * 0.45 + 20000 * 0.4 + 20000 * 0.35 + 20000 * 0.3 + 20000 * 0.25 + 15000 * 0.2 + 3000 * 0.15 + 1500 * 0.1 + 500 * 0.05;
     }
 
 }
@@ -105,8 +105,8 @@ int read()
     fseek(fp,0,SEEK_SET);
     fseek(fp,0,SEEK_END);
 
-    long longBytes=ftell(fp)/sizeof(struct zggz);// 统计人数
-    n=longBytes;
+    long longBytes = ftell(fp) / sizeof(struct zggz);// 统计人数
+    n = longBytes;
     fseek(fp,0,SEEK_SET);
     if ( fp == NULL )
 	{
@@ -140,7 +140,7 @@ int find()
 	scanf("%s", gonghao);
 	for(int i=0; i<n; i++)
 	{
-		if(!strcmp(gonghao,zggz[i].num))
+		if(!strcmp(gonghao, zggz[i].num))
 		{
 			printf("工号：");
 			printf("%s",zggz[i].num);
@@ -237,25 +237,25 @@ void del()
 	scanf("%s", gonghao);
 	for(int i=0; i<n; i++)
 	{
-		if(!strcmp(gonghao,zggz[i].num))
+		if(!strcmp(gonghao, zggz[i].num))
 		{
 			printf("是否确认删除该记录?（请输入y/n）\n");
 			scanf("%s", choose);
-			if(!strcmp(choose,"y"))
+			if(!strcmp(choose, "y"))
 			{
 				printf("开始删除\n");
 				for(int j=i;j<n-1;j++)
 				{
-					strcpy(zggz[j].num,zggz[j+1].num);
-					strcpy(zggz[j].name,zggz[j+1].name);
-					zggz[j].pay_gw=zggz[j+1].pay_gw;
-					zggz[j].pay_sf=zggz[j+1].pay_sf;
-					zggz[j].pay_xiaoji=zggz[j+1].pay_xiaoji;
-					zggz[j].pay_xinji=zggz[j+1].pay_xinji;
-					zggz[j].pay_yf=zggz[j+1].pay_yf;
-					zggz[j].pay_zw=zggz[j+1].pay_zw;
+					strcpy(zggz[j].num, zggz[j+1].num);
+					strcpy(zggz[j].name, zggz[j+1].name);
+					zggz[j].pay_gw = zggz[j+1].pay_gw;
+					zggz[j].pay_sf = zggz[j+1].pay_sf;
+					zggz[j].pay_xiaoji = zggz[j+1].pay_xiaoji;
+					zggz[j].pay_xinji = zggz[j+1].pay_xinji;
+					zggz[j].pay_yf = zggz[j+1].pay_yf;
+					zggz[j].pay_zw = zggz[j+1].pay_zw;
 				}
-				n=n-1;
+				n = n-1;
 				printf("删除完成，请及时保存\n");
 			}
 			else
@@ -290,11 +290,11 @@ void add()
 		scanf("%f", &zggz[i].pay_zw);
 		printf("\n绩效工资：\n");
 		scanf("%f", &zggz[i].pay_xiaoji);
-		zggz[i].pay_yf=zggz[i].pay_gw+zggz[i].pay_xiaoji+zggz[i].pay_xinji+zggz[i].pay_zw;
+		zggz[i].pay_yf = zggz[i].pay_gw + zggz[i].pay_xiaoji + zggz[i].pay_xinji + zggz[i].pay_zw;
 		grsds(i);
-		zggz[i].pay_sf=zggz[i].pay_yf-zggz[i].duty;
+		zggz[i].pay_sf = zggz[i].pay_yf - zggz[i].duty;
 	}
-		n=n+add_num;
+		n = n + add_num;
 
 };
 
